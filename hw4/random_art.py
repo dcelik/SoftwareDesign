187686a02fcb8b1737f63030c735ca95433cb948
Deniz Celik <dcelik@TurkishDelight.(none)>
2014-02-13 15:32:07 -0500

    added fibonacci recursion w/ and w/o memoization; added basic functoins for randomar

--- hw4/random_art.py
+++ hw4/random_art.py
@@ -8,15 +8,31 @@ Created on Tue Feb 11 11:34:57 2014
 # you do not have to use these particular modules, but they may help
 from random import randint
 import Image
+import math
 
 def build_random_function(min_depth, max_depth):
     # your doc string goes here
-
+    return
     # your code goes here
+    
+def prod(a,b):
+    return a*b
+
+def cos_pi(a):
+    return math.cos(math.pi*a)
+    
+def sin_pi(a):
+    return math.sin(math.pi*a)
+
+def x(a,b):
+    return a
+
+def y(a,b):
+    return b
 
 def evaluate_random_function(f, x, y):
     # your doc string goes here
-
+    return 
     # your code goes here
 
 def remap_interval(val, input_interval_start, input_interval_end, output_interval_start, output_interval_end):
